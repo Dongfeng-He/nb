a76a58e22f884b5e80a8950f3b9e6ded9841a296
hedongfeng <hedongfeng@qingting.fm>
2019-06-03 18:00:17 +0800

    m

--- train_series.py
+++ train_series.py
@@ -74,7 +74,7 @@ if __name__ == "__main__":
     print("start to train: train14_identity_binary_or")
     trainer = Trainer(data_dir=data_dir, model_name="train14_identity_binary_or", debug_mode=debug_mode)
     trainer.train(epochs=5, batch_size=512)
-    """
+
     from train.train23_aux_identity_2_layer_hidden import Trainer
     print("start to train: train23_aux_identity_2_layer_hidden")
     trainer = Trainer(data_dir=data_dir, model_name="train23_aux_identity_2_layer_hidden", debug_mode=debug_mode)
@@ -113,6 +113,11 @@ if __name__ == "__main__":
     from train.train22_identity_2_layer_hidden2 import Trainer
     print("start to train: train22_identity_2_layer_hidden2")
     trainer = Trainer(data_dir=data_dir, model_name="train22_identity_2_layer_hidden2", debug_mode=debug_mode)
+    trainer.train(epochs=5, batch_size=512)
+    """
+    from train.train21_focal_loss import Trainer
+    print("start to train: train21_focal_loss")
+    trainer = Trainer(data_dir=data_dir, model_name="train21_focal_loss", debug_mode=debug_mode)
     trainer.train(epochs=5, batch_size=512)
 
 
