50ddea4a3afa030eb881dabd00fedc161e7315b1
hedongfeng <hedongfeng@qingting.fm>
2019-06-22 09:39:57 +0800

    m

--- train_series.py
+++ train_series.py
@@ -141,8 +141,7 @@ if __name__ == "__main__":
     """
     from train_bert.train5_np_task import Trainer
     print("start to train_bert: train5_np_task")
-    debug_mode = True
-    trainer = Trainer(data_dir=data_dir, model_name="train5_np_task", epochs=3, batch_size=48, base_batch_size=24, part=1., seed=1234, debug_mode=debug_mode)
+    trainer = Trainer(data_dir=data_dir, model_name="train5_np_task", epochs=3, batch_size=48, base_batch_size=24, part=0.3, seed=1234, debug_mode=debug_mode)
     trainer.train()
     del trainer
     gc.collect()
