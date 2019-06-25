7b29a3aab4aac797e17e1f1dac170e6f8d9ccfbb
hedongfeng <hedongfeng@qingting.fm>
2019-06-25 15:46:21 +0800

    m

--- train_series.py
+++ train_series.py
@@ -139,6 +139,7 @@ if __name__ == "__main__":
     del trainer
     gc.collect()
     """
+    # d
     from train_bert.train12_aux import Trainer
     print("start to train_bert: train12_aux")
     debug_mode = True
